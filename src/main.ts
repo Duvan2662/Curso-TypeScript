302589aee07c72475fbfcc69b3b4a8b706da6eff
Duvan <duvanriano2626@gmail.com>
2024-07-02 16:45:33 -0500

    Tarea de creacion de interfaces de un objeto

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 // import './topic/01-basic-types';
 // import './topic/02-object-interfaces';
-import './topic/03-functions';
+//import './topic/03-functions';
+import './topic/04-homeworks-types';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
