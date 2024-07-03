8519115c1fbca554401b5633d1790336ce77d789
Duvan <duvanriano2626@gmail.com>
2024-07-02 19:45:34 -0500

    Decoradores

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 // import './topic/01-basic-types';
 // import './topic/02-object-interfaces';
-//import './topic/03-functions';
+// import './topic/03-functions';
 // import './topic/04-homeworks-types';
 // import './topic/05-basic-destructuring';
 // import './topic/06-function-destructuring';
 // import './topic/07-import-export';
 // import './topic/08-classes';
-import './topic/09-generics';
+// import './topic/09-generics';
+import './topic/10-decorators';
+
 
 
 
